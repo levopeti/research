6a0b120a941a2e9507436702754a06028e09657a
Levente Peto <leviskocka@gmail.com>
2018-10-19 15:38:59 +0200

    numpy backprop

--- genetic_algorithm/neural_network.py
+++ genetic_algorithm/neural_network.py
@@ -1,83 +1,41 @@
-import tensorflow as tf
 import numpy as np
 import time
+import tensorflow as tf
+from scipy import signal
+from sklearn.metrics import log_loss
 import os
+
+np.random.seed(int(time.time()))
 
 
 class NeuralNet(object):
     def __init__(self):
+        self.num_class = 10
+        self.learning_rate = 0.1
+
         mnist = tf.keras.datasets.mnist
         (x_train, y_train), (x_test, y_test) = mnist.load_data()
         x_train, x_test = x_train / 255.0, x_test / 255.0
         x_train = np.reshape(x_train, (-1, 28 * 28))
         x_test = np.reshape(x_test, (-1, 28 * 28))
 
-        x = np.append(x_train, x_test, axis=0)
-        y_ = np.append(y_train, y_test)
-        self.y_ = np.int64(y_)
+        self.X = np.append(x_train, x_test, axis=0)
+        self.Y = np.eye(self.num_class)[np.append(y_train, y_test)]  # one hot vectors
 
-        self.W = None
-        self.b = None
-
-        self.x = tf.constant(x)
-        self.y = tf.constant(self.y_)
-
-        self.gpu_counter = 0
-
-        self.sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+        self.W = (np.random.rand(784, 10) * 2) - 1
+        self.b = (np.random.rand(10) * 0.56) - 1
 
     def __del__(self):
-        self.sess.close()
+        pass
 
     def set_w(self, tensor):
-        self.W = tensor     # shape (784, 10)
+        pass     # shape (784, 10)
 
     def set_b(self, vector):
-        self.b = vector         # shape (10,)
+        pass         # shape (10,)
 
     def evaluate(self, acc=False):
-        #start = time.time()
-        #print(self.gpu_counter)
-        self.gpu_counter += 1
-        #gpu_options = tf.GPUOptions(allow_growth=True, visible_device_list=str(int(self.gpu_counter)))
 
-        #os.environ['CUDA_VISIBLE_DEVICES'] = str(int(self.gpu_counter))
-
-        if self.gpu_counter == 3:
-            self.gpu_counter = 0
-
-        #with tf.device('/gpu:{}'.format(self.gpu_counter)):
-
-        # sess = None
-        b = False
-        # loss = None
-        # pred = None
-        while b:
-            try:
-                b = False
-                print(self.gpu_counter)
-                gpu_options = tf.GPUOptions(allow_growth=True, visible_device_list=str(int(self.gpu_counter)))
-                fc = tf.contrib.layers.fully_connected(self.x, 10, tf.nn.relu, weights_initializer=tf.constant_initializer(self.W), biases_initializer=tf.constant_initializer(self.b))
-                loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.y, logits=fc))
-                pred = tf.argmax(fc, 1)
-                sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True, gpu_options=gpu_options))
-
-            except ValueError:
-                #print(666)
-                self.gpu_counter += 1
-                if self.gpu_counter == 3:
-                    self.gpu_counter = 0
-                b = True
-
-        fc = tf.contrib.layers.fully_connected(self.x, 10, tf.nn.relu, weights_initializer=tf.constant_initializer(self.W), biases_initializer=tf.constant_initializer(self.b))
-        loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.y, logits=fc))
-        pred = tf.argmax(fc, 1)
-
-        self.sess.run(tf.global_variables_initializer())
-        loss, predicted = self.sess.run([loss, pred])
-
-        #end = time.time()
-        #print('eval: ', end - start, 's')
         if acc:
             accurate = self.accurate_func(predicted)
 
@@ -87,21 +45,34 @@ class NeuralNet(object):
             #sess.close()
             return loss, 0
 
+    # one epoch
+    def train_step(self):
+        global_loss = 0
+        predicted_values = []
+        for i in range(len(self.X)):
+            layer_fc = self.X[i].dot(self.W) + self.b
+            layer_fc_act = self.log(layer_fc)
+            softmax_v = self.softmax(layer_fc_act)
+
+            loss = log_loss(self.Y[i], softmax_v)
+            global_loss += loss
+            predicted_values.append(np.argmax(softmax_v))
+
+            grad_fc_part_1 = layer_fc_act - self.Y[i]
+            grad_fc_part_2 = self.d_log(layer_fc)
+            grad_fc_part_3 = self.X[i]
+            grad_fc = grad_fc_part_3.T.dot(grad_fc_part_1 * grad_fc_part_2)
+
+            self.W = self.W - grad_fc * self.learning_rate
+            self.b = self.b - grad_fc_part_1 * self.learning_rate
+
+            return global_loss, np.array(predicted_values)
+
     def base_line(self, epochs):
-        fc = tf.contrib.layers.fully_connected(self.x, 10, tf.nn.relu)
-        loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.y, logits=fc))
-        train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
-        pred = tf.argmax(fc, 1)
-
-        tf.set_random_seed(1234)
-
-        sess = tf.Session()
-        sess.run(tf.global_variables_initializer())
 
         for i in range(epochs):
                 print('EPOCH', i + 1)
-                _, predicted, loss_value = sess.run([train_op, pred, loss])
-                #print("Loss: ", loss_value)
+                loss_value, predicted = self.train_step()
                 print("Accurate: {0:.2f}%".format(self.accurate_func(predicted) * 100))
                 print()
 
@@ -110,14 +81,31 @@ class NeuralNet(object):
 
         for i in range(pred.shape[0]):
 
-            if pred[i] == self.y_[i]:
+            if pred[i] == np.argmax(self.Y[i]):
                 goal += 1
         return goal / pred.shape[0]
 
+    @staticmethod
+    def softmax(x):
+        """Compute softmax values for each sets of scores in x."""
+        e_x = np.exp(x - np.max(x))
+        return e_x / e_x.sum()
 
-# nn = NeuralNet()
-#
-# nn.base_line()
+    def tanh(self, x):
+        return np.tanh(x)
+
+    def d_tanh(self, x):
+        return 1 - np.tanh(x) ** 2
+
+    def log(self, x):
+        return 1 / (1 + np.exp(-1 * x))
+
+    def d_log(self, x):
+        return self.log(x) * (1 - self.log(x))
+
+nn = NeuralNet()
+
+nn.base_line(100)
 #
 # W = np.ones(shape=(784, 10))
 # b = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]

a5f36101ef9b9ab9556895038f3c49df8b9bbbb9
Levente Peto <leviskocka@gmail.com>
2018-11-08 22:02:12 +0100

    nn_cm and nn ()()

--- genetic_algorithm/neural_network.py
+++ genetic_algorithm/neural_network.py
@@ -3,38 +3,43 @@ import tensorflow as tf
 import time
 import pickle
 import os
+from abc import ABC, abstractmethod
+from scipy import signal
 
 np.random.seed(int(time.time()))
 
 
 class NeuralNet(object):
-    def __init__(self):
-        self.num_class = 10
+    def __init__(self, input, output):
         self.learning_rate = 0.1
+        self.loss = None
+        self.error = None
 
-        loss = "MSE"
-        self.loss = self.loss_func(loss)
-        self.error = self.error_func(loss)
+        self.X = None
+        self.Y = None
 
-        mnist = tf.keras.datasets.mnist
-        (x_train, y_train), (x_test, y_test) = mnist.load_data()
-        x_train, x_test = x_train / 255.0, x_test / 255.0
-        x_train = np.reshape(x_train, (-1, 28 * 28))
-        x_test = np.reshape(x_test, (-1, 28 * 28))
+        self.input = input
+        self.output = output
 
-        self.X = np.array(np.append(x_train, x_test, axis=0))
-        self.Y = np.eye(self.num_class)[np.append(y_train, y_test)]  # one hot vectors
-
-        self.model = []
-        self.build_model()
+        self.model = None
 
     def __del__(self):
         pass
 
-    def build_model(self):
+    def build_model(self, loss="MSE", learning_rate=0.1):
         print("Build the model...\n")
-        self.model.append(Layer("fc", len(self.X[0]), 10, self.learning_rate, "sigmoid"))
-        # self.model.append(Layer("fc", 256, 10, self.learning_rate, "sigmoid"))
+
+        self.learning_rate = learning_rate
+        self.loss = self.loss_func(loss)
+        self.error = self.error_func(loss)
+
+        # def compile(layer):
+        #     if layer:
+        #         for next_layer in layer.next_layer:
+        #             next_layer.learning_rate = self.learning_rate
+        #             compile(next_layer)
+        #
+        # compile(self.input)
 
     def set_weights(self, individual):
         self.W = np.reshape(np.array(individual[:7840]), (784, 10))  # shape (784, 10)
@@ -59,8 +64,9 @@ class NeuralNet(object):
 
         for i in range(len(self.X)):
 
-            # forward
-            o = self.forward(i)
+            # forward process
+            self.forward(i)
+            o = self.output.output
 
             loss, predicted_value = self.loss(o, self.Y[i])
             predicted_values.append(predicted_value)
@@ -76,40 +82,46 @@ class NeuralNet(object):
 
         for i in range(len(self.X)):
 
-            # forward
-            o = self.forward(i)
-
+            # forward process
+            start = time.time()
+            self.forward(i)
+            o = self.output.output
+            # print("Time of forward: {}s\n".format(time.time() - start))
             error = self.error(o, self.Y[i])
 
-            # backward
+            # backward process
+            start = time.time()
             self.backward(error, i)
+            # print("Time of backward: {}s\n".format(time.time() - start))
+            # input()
 
     def forward(self, i):
-        data = self.X[i]
-        for layer in self.model:
-            data = layer.forward(data)
-        return data
+        self.input.forward_process(self.X[i])
+        # for layer in self.model:
+        #     data = layer.forward(data)
+        # return data
 
     def backward(self, output_bp, i):
         # for the first layer the output_bp = error
-        for j in range(len(self.model))[::-1]:
-            layer = self.model[j]
-            if j == 0:
-                output_bp = layer.backward(self.X[i], output_bp)
-            else:
-                output_bp = layer.backward(self.model[j - 1].output, output_bp)
+        self.output.backward_process(output_bp)
 
-    def base_line(self, epochs):
+        # for j in range(len(self.model))[::-1]:
+        #     layer = self.model[j]
+        #     if j == 0:
+        #         output_bp = layer.backward(self.X[i], output_bp)
+        #     else:
+        #         output_bp = layer.backward(self.model[j - 1].output, output_bp)
+
+    def train(self, X, Y, epochs):
+        self.X = X
+        self.Y = Y
+
         print("Start training the model...\n")
 
         for i in range(epochs):
             start = time.time()
             self.train_step()
             loss_value, accurate = self.evaluate()
-            # print("max of W: {0:.2f}".format(np.amax(self.W)))
-            # print("min of W: {0:.2f}\n".format(np.amin(self.W)))
-            # print("max of b: {0:.2f}".format(np.amax(self.b)))
-            # print("min of b: {0:.2f}\n".format(np.amin(self.b)))
 
             print("EPOCH", i + 1, "\tAccurate: {0:.2f}%\t".format(accurate * 100), "Loss: {0:.2f}\t".format(loss_value), "ETA: {0:.2f}s\n".format(time.time() - start))
             if i == 30:
@@ -192,54 +204,43 @@ class NeuralNet(object):
         return grad
 
 
-class Layer(object):
-    def __init__(self, layer_type, input_size, output_size, learning_rate, activation):
-        self.layer_type = layer_type
-        self.input_size = input_size
-        self.output_size = output_size
+class Layer(ABC):
+    def __init__(self, activation="sigmoid", learning_rate=0.1, prev_layer=None):
+        self.input_size = None
+        self.output_size = None
         self.learning_rate = learning_rate
 
         self.act = self.act_func(activation)
         self.d_act = self.d_act_func(activation)
 
         self.output = None
+        self.output_bp = None
 
         self.W = None
         self.b = None
         self.z = None
 
-        self.forward = None
-        self.backward = None
+        self.prev_layer = prev_layer
+        self.next_layer = []
 
-        if self.layer_type == "fc":
-            self.W = (np.random.rand(self.input_size, self.output_size) * 1) - 0.5
-            self.b = (np.random.rand(self.output_size) * 1) - 0.5
-            self.forward = self.dense_fw
-            self.backward = self.dense_bw
+        self.set_prev_layer()
 
-    def dense_fw(self, x):
-        """Fully connected layer forward process."""
-        self.z = np.add(np.dot(x, self.W), self.b)
-        self.output = self.act(self.z)
-        return self.output
+        # convention of input shape
 
-    def dense_bw(self, input_layer, input_error):
-        """Fully connected layer backward process"""
-        d_act_z = self.d_act(self.z)
+    def set_prev_layer(self):
+        self.prev_layer.set_next_layer(self)
+        self.input_size = self.prev_layer.output_size
 
-        delta_b = np.multiply(input_error, d_act_z)
-        delta_b = delta_b.reshape((1, -1))
+    @abstractmethod
+    def forward_process(self):
+        pass
 
-        x = input_layer.reshape((1, -1))
-        x = np.transpose(x)
-        delta_W = np.dot(x, delta_b)
+    @abstractmethod
+    def backward_process(self, input_error):
+        pass
 
-        output_bp = np.dot(delta_b, np.transpose(self.W))
-
-        self.W = np.subtract(self.W, delta_W * self.learning_rate)
-        self.b = np.subtract(self.b, delta_b * self.learning_rate)
-
-        return output_bp
+    def set_next_layer(self, layer):
+        self.next_layer.append(layer)
 
     def act_func(self, type):
         if type == "tanh":
@@ -261,26 +262,154 @@ class Layer(object):
         elif type == "relu":
             return self.d_relu
 
-    def tanh(self, x):
+    @staticmethod
+    def tanh(x):
         return np.tanh(x)
 
-    def d_tanh(self, x):
+    @staticmethod
+    def d_tanh(x):
         return 1 - np.tanh(x) ** 2
 
-    def log(self, x):
+    @staticmethod
+    def log(x):
         return 1 / (1 + np.exp(-1 * x))
 
     def d_log(self, x):
         return self.log(x) * (1 - self.log(x))
 
-    def relu(self, x):
+    @staticmethod
+    def relu(x):
         return np.maximum(x, 0)
 
-    def d_relu(self, x):
+    @staticmethod
+    def d_relu(x):
         return np.where(x > 0, 1, 0)
 
 
+class Dense(Layer):
+    def __new__(cls, units, activation="sigmoid", learning_rate=0.1):
+        def set_prev_layer(layer):
+            instance = super(Dense, cls).__new__(cls)
+            instance.__init__(units, activation="sigmoid", learning_rate=learning_rate, prev_layer=layer)
+            return instance
+        return set_prev_layer
+
+    def __init__(self, units, activation="sigmoid", learning_rate=0.1, prev_layer=None):
+        super().__init__(activation, learning_rate, prev_layer)
+        self.output_size = (1, units)
+
+        self.W = (np.random.rand(self.input_size[1], self.output_size[1]) * 1) - 0.5
+        self.b = (np.random.rand(self.output_size[1]) * 1) - 0.5
+        # self.b = np.zeros(self.output_size)
+
+        log = "Dense layer with {} parameters.\nInput size: {}\nOutput size: {}\n".format((self.input_size[1] + 1) * self.output_size[1], self.input_size[1], self.output_size[1])
+        print(log)
+
+    def forward_process(self):
+        """Fully connected layer forward process."""
+        x = self.prev_layer.output
+        self.z = np.add(np.dot(x, self.W), self.b)
+        self.output = self.act(self.z)
+        for layer in self.next_layer:
+            layer.forward_process()
+
+    def backward_process(self, input_error):
+        """Fully connected layer backward process"""
+        d_act_z = self.d_act(self.z)
+
+        delta_b = np.multiply(input_error, d_act_z)
+        delta_b = delta_b.reshape((1, -1))
+        x = self.prev_layer.output
+        x = np.transpose(x)
+        delta_W = np.dot(x, delta_b)
+
+        self.output_bp = np.dot(delta_b, np.transpose(self.W))
+        self.W = np.subtract(self.W, delta_W * self.learning_rate)
+        self.b = np.subtract(self.b, delta_b * self.learning_rate)
+
+        self.prev_layer.backward_process(self.output_bp)
+
+# class Conv2d(Layer):
+#     def __init__(self, input_size, output_size, learning_rate, activation, kernel_size, number_of_kernel):
+#         super().__init__(input_size, output_size, learning_rate, activation)
+#         self.kernel_size = kernel_size
+#         self.number_of_kernel = number_of_kernel
+#
+#         self.W = (np.random.rand(self.number_of_kernel, self.kernel_size, self.kernel_size) * 1) - 0.5
+#         self.b = (np.random.rand(self.number_of_kernel) * 1) - 0.5
+#         # self.b = np.zeros(self.output_size)
+#         self.forward = self.forward_process
+#         self.backward = self.backward_process
+#
+#     def forward_process(self, x):
+#         """2d convolution layer forward process."""
+#         # TODO: Bias
+#         conv_input = x
+#         self.z = []
+#
+#         for kernel in self.W:
+#             self.z.append(signal.convolve2d(conv_input, kernel, mode="same"))
+#         self.z = np.array(self.z)
+#         self.output = self.act(self.z)
+#
+#         return self.output
+#
+#     def backward_process(self, input_layer, input_error):
+#         """2d convolution layer backward process"""
+#         # TODO: Bias
+#         d_act_z = self.d_act(self.z)
+#
+#         for kernel in self.W:
+#
+#
+# class Concat:
+#     pass
+
+
+class Input(object):
+    def __init__(self, input_size: tuple):
+        self.output_size = input_size
+
+        self.output = None
+        self.next_layer = []
+
+    def set_next_layer(self, layer):
+        self.next_layer.append(layer)
+
+    def forward_process(self, x):
+        self.output = x
+        for layer in self.next_layer:
+            layer.forward_process()
+
+    def backward_process(self, input_error):
+        pass
+
+
 if __name__ == "__main__":
-    nn = NeuralNet()
-    nn.base_line(60)
+    num_class = 10
+    mnist = tf.keras.datasets.mnist
+    fashion = False
+    if fashion:
+        from keras.datasets import fashion_mnist
+
+        mnist = fashion_mnist
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+    x_train = np.reshape(x_train, (-1, 1, 28 * 28))
+    x_test = np.reshape(x_test, (-1, 1, 28 * 28))
+
+    X = np.array(np.append(x_train, x_test, axis=0))
+    Y = np.eye(num_class)[np.append(y_train, y_test)]  # one hot vectors
+
+    ip = Input(input_size=(1, 784))
+    x = Dense(units=512, activation="sigmoid")(ip)
+    x = Dense(units=128, activation="sigmoid")(x)
+    op = Dense(units=10, activation="sigmoid")(x)
+
+    nn = NeuralNet(ip, op)
+    nn.build_model(loss="MSE", learning_rate=0.1)
+    nn.train(X, Y, epochs=60)
     nn.save_weights()
+
+

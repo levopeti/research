18c7bb300766e896809104648bbb00cb779f0e97
Levente Peto <leviskocka@gmail.com>
2019-04-01 13:43:33 +0200

    pyplot without display

--- genetic_algorithm/elements/callbacks.py
+++ genetic_algorithm/elements/callbacks.py
@@ -1,5 +1,9 @@
 from abc import ABC
 import os
+import matplotlib as mpl
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using non-interactive Agg backend')
+    mpl.use('Agg')
 import yaml
 import pickle
 import numpy as np
